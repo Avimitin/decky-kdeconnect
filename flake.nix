1f1f6a7f6c990439409456f5ce6d07cda2350f5c
Avimitin <dev@avimit.in>
2024-01-01 22:48:38 +0800

    Add flakes
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Generic devshell setup";
+
+  inputs = {
+    # The nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Utility functions
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      pkgsForSys = system: import nixpkgs { inherit system; };
+      perSystem = (system:
+        let
+          pkgs = pkgsForSys system;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodePackages.pnpm
+            ];
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+        });
+    in
+    {
+      # Other system-independent attr
+    } //
+
+    flake-utils.lib.eachDefaultSystem perSystem;
+}
