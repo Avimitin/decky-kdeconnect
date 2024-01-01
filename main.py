f1433cc5af65733479a4b2ba730f4ea10fac3ee7
Avimitin <dev@avimit.in>
2024-01-01 22:50:26 +0800

    Add basic systemd service operation
    Signed-off-by: Avimitin <dev@avimit.in>

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+import subprocess
 
 # The decky plugin module is located at decky-loader/plugin
 # For easy intellisense checkout the decky-loader code one directory up
 # or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
+
+KDECONNECTD_UNIT_NAME = "decky-kde-connectd"
 
 
 class Plugin:
@@ -11,31 +14,42 @@ class Plugin:
     async def add(self, left, right):
         return left + right
 
+    async def list_avaliable(self):
+        outputs = subprocess.run(["kdeconnect-cli", "-a"], capture_output=True)
+        return
+
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        decky_plugin.logger.info("Hello World!")
+        decky_plugin.logger.info("Try to run kdeconnectd")
+        outputs = subprocess.run(
+            [
+                "systemd-run",
+                "--user",
+                "--unit",
+                KDECONNECTD_UNIT_NAME,
+                "/usr/lib/kdeconnectd",
+            ],
+            check=False,
+            capture_output=True,
+            encoding="UTF-8",
+        )
+        if outputs.returncode != 0:
+            decky_plugin.logger.error(f"fail to start systemd: {outputs.stderr}")
+        return outputs.returncode == 0
 
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
-        decky_plugin.logger.info("Goodbye World!")
-        pass
+        decky_plugin.logger.info("Stopping kdeconnectd")
+        outputs = subprocess.run(
+            ["systemctl", "--user", "stop", KDECONNECTD_UNIT_NAME],
+            check=False,
+            capture_output=True,
+        )
+        if outputs.returncode != 0:
+            decky_plugin.logger.error(f"fail to stop kdeconnectd: {outputs.stderr}")
+        # TODO: reload systemd?
+        return outputs.returncode == 0
 
     # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky_plugin.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
